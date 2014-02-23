b621e559db1f0ab232336c714e04f671bcc4e13f
Mike James <m.l.james@me.com>
2014-02-23 16:27:24 +0000

    Rover can now reverse. Tests Passing.

--- MarsRover/Implementations/Reverse.cs
+++ MarsRover/Implementations/Reverse.cs
@@ -8,13 +8,13 @@ namespace MarsRover.Implementations
 {
     public class Reverse : ICommand
     {
-        private readonly int _moveForwards = 1;
+        private readonly int _reverseBy = -1;
         
         public int CommandValue
         {
             get
             {
-                return _moveForwards;
+                return _reverseBy;
             }
         }
 
